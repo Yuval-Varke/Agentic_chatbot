94b0019ff0bde56ebba0fce909c22dc586b06a24
Yuval Varke <varke.vishu01@gmail.com>
2026-05-13 10:50:11 +0530

    Fixed setup graph node issue; Completed Basic chatbot ✅

--- src/langgraphagenticai/graph/graph_builder.py
+++ src/langgraphagenticai/graph/graph_builder.py
@@ -22,5 +22,13 @@ class GraphBuilder:
         self.graph_builder.add_edge(START,"chatbot")
         self.graph_builder.add_edge("chatbot",END)
 
+    def setup_graph(self, usecase: str):
+        """
+        Sets up the graph for the selected use case.
+        """
+        if usecase == "Basic Chatbot":
+            self.basic_chatbot_build_graph()
+
+        return self.graph_builder.compile()
 
 
